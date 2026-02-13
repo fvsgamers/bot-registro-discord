dc4763e1410d53781018923e20f0c62ed11cfe87
fvsgamers <fvsgamers@gmail.com>
2026-02-13 19:43:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -198,5 +198,22 @@ async def on_ready():
     bot.add_view(RegistroView())
 
 # ================= START ================= #
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot está online!"
+
+def run():
+    app.run(host='0.0.0.0', port=8000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 
 bot.run(TOKEN)
