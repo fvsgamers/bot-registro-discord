37050485c964a26809ec86681daa93278da5464a
fvsgamers <fvsgamers@gmail.com>
2026-02-13 18:56:51 -0300

    upload inicial

---
+++ config.py
@@ -0,0 +1,38 @@
+CARGOS = {
+    "staff": {
+        "nome": "🛡️ Staff",
+        "id": 1459264090105970696,
+        "prefixo": "[STAFF]"
+    },
+    "mod": {
+        "nome": "⚙️ MOD",
+        "id": 1471731349374242990,
+        "prefixo": "[MOD]"
+    },
+    "vip": {
+        "nome": "💎 VIP",
+        "id": 1459264090105970694,
+        "prefixo": "[VIP]"
+    },
+    "streamer": {
+        "nome": "🎮 Streamer",
+        "id": 1459264090105970695,
+        "prefixo": ""
+    },
+    "membro": {
+        "nome": "👤 Membro",
+        "id": 1459264090105970693,
+        "prefixo": "[MEMBRO]"
+    },
+    "visitante": {
+        "nome": "🏞 Visitante",
+        "id": 1459264089762304115,
+        "prefixo": ""
+    }
+}
+
+CARGO_APROVADOR_ID = 1459264090105970696
+CATEGORIA_REGISTROS_ID = 1471732973467468008
+
+# prioridade do nickname
+PRIORIDADE_NICK = ["staff", "mod", "vip", "membro"]
